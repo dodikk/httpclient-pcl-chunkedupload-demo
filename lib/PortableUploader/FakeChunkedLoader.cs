d7f12b5c3b371f9ddc004231248d39512b57d72e
dodikk <dodikk88.reg@gmail.com>
2014-10-31 12:45:23 +0200

    fixed some syntax errors

---
+++ lib/PortableUploader/FakeChunkedLoader.cs
@@ -0,0 +1,74 @@
+﻿namespace PortableUploader
+{
+  using System;
+  using System.Threading.Tasks;
+  using System.Net.Http;
+  using System.IO;
+  using System.Net.Http.Headers;
+
+
+  public class FakeChunkedLoader
+  {
+    public async Task<Stream> UploadAsync(
+      Stream data, 
+      string fileName,
+      string mimeType,
+      TestTweaks tweaks)
+    {
+      string url = "https://scmobileteam.sitecoretest.net/-/item/v1/sitecore/Media%20Library/NewItem";
+      string userName = "sitecore\\uploaderuser";
+      string password = "uploaderuser";
+
+
+      HttpRequestMessage result = new HttpRequestMessage(HttpMethod.Post, url);
+
+      MultipartFormDataContent multiPartContent = new MultipartFormDataContent();
+
+      StreamContent strContent = new StreamContent(data);
+
+      ContentDispositionHeaderValue cdHeaderValue = new ContentDispositionHeaderValue("data");
+      cdHeaderValue.FileName = "\"" + fileName + "\"";
+      cdHeaderValue.Name = "\"datafile\"";
+      strContent.Headers.ContentDisposition = cdHeaderValue;
+
+      if (null != mimeType)
+      {
+        strContent.Headers.ContentType = MediaTypeHeaderValue.Parse(mimeType);
+      }
+
+      multiPartContent.Add(strContent);
+      result.Content = multiPartContent;
+
+
+
+      // TODO : find a right way of setting the chunked encoding
+      {
+        if (tweaks.ShouldAddChunkedEncodingProperty)
+        {
+          // causes System.Net.ProtocolViolationException
+          result.Headers.TransferEncodingChunked = true;
+        }
+        else if (tweaks.ShouldAddChunkedEncodingExplicitly)
+        {
+          // causes System.Net.ProtocolViolationException
+          result.Headers.Add("Transfer-Encoding", "chunked");
+        }
+      }
+
+
+      // authentication
+      {
+        result.Headers.Add("X-Scitemwebapi-Username", userName);
+        result.Headers.Add("X-Scitemwebapi-Password", password);
+      }
+
+
+      HttpClient sender = new HttpClient();
+      HttpResponseMessage response = await sender.SendAsync(result);
+
+      return await response.Content.ReadAsStreamAsync();
+    }
+  
+  }
+}
+
